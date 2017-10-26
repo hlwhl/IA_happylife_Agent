f115741bc9c1adb420830e2162936ee9aa3211a3
hlwhl <h2113191@gmail.com>
2017-10-26 14:21:49 +0100

    update basic methods

--- ourAgent/src/agent.java
+++ ourAgent/src/agent.java
@@ -1,12 +1,24 @@
+import negotiator.AgentID;
 import negotiator.actions.Action;
 import negotiator.parties.AbstractNegotiationParty;
+import negotiator.parties.NegotiationInfo;
 
 import java.util.List;
 
 public class agent extends AbstractNegotiationParty {
     @Override
+    public void init(NegotiationInfo info) {
+        super.init(info);
+    }
+
+    @Override
     public Action chooseAction(List<Class<? extends Action>> list) {
         return null;
+    }
+
+    @Override
+    public void receiveMessage(AgentID sender, Action act) {
+        super.receiveMessage(sender, act);
     }
 
     @Override
