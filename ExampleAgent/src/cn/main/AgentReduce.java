8384fa7317650676acdc4b35a3ece88ba9565b9c
dahuangshebao <huangyankai1024@163.com>
2017-11-25 13:57:01 +0000

    提交忽略的文件格式

---
+++ ExampleAgent/src/cn/main/AgentReduce.java
@@ -0,0 +1,62 @@
+package cn.main;
+
+import negotiator.AgentID;
+import negotiator.Bid;
+import negotiator.actions.Accept;
+import negotiator.actions.Action;
+import negotiator.actions.Offer;
+import negotiator.parties.AbstractNegotiationParty;
+import negotiator.parties.NegotiationInfo;
+import negotiator.timeline.TimeLineInfo;
+
+import java.util.List;
+
+@SuppressWarnings("serial")
+public class AgentReduce extends AbstractNegotiationParty {
+
+    private Bid lastReceivedOffer;
+    private Bid myLastOffer;
+    private double thresholdUtility;
+    private TimeLineInfo timeline;
+
+    @Override
+    public void init(NegotiationInfo info) {
+        super.init(info);
+        thresholdUtility=1;
+        this.timeline=info.getTimeline();
+    }
+
+    @Override
+    public Action chooseAction(List<Class<? extends Action>> list) {
+        updateThresholdUtility();
+        if(lastReceivedOffer!=null && this.utilitySpace.getUtility(lastReceivedOffer)>=thresholdUtility){
+            return new Accept(this.getPartyId(),this.lastReceivedOffer);
+        }else{
+            do {
+                myLastOffer=generateRandomBid();
+            }while (this.getUtility(myLastOffer)<thresholdUtility);
+            return new Offer(this.getPartyId(),myLastOffer);
+        }
+    }
+
+    @Override
+    public void receiveMessage(AgentID sender, Action act) {
+        super.receiveMessage(sender, act);
+        if(act instanceof Offer){
+            Offer offer= (Offer) act;
+            lastReceivedOffer=offer.getBid();
+        }
+    }
+
+
+    private void updateThresholdUtility(){
+        double currenttime=timeline.getCurrentTime();
+        this.thresholdUtility=1.0D-Math.pow(1.1,timeline.getCurrentTime()-180);
+    }
+
+    @Override
+    public String getDescription() {
+        return "AgentReduce";
+    }
+}
+
