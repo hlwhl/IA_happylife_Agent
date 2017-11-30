479131d9fbdaf211b6c8523597fa2ad06a748aed
hlwhl <h2113191@gmail.com>
2017-11-30 16:14:57 +0000

    加入agree随时间变化阈值条件

--- ExampleAgent/src/cn/main/MyNegotiationStrategy.java
+++ ExampleAgent/src/cn/main/MyNegotiationStrategy.java
@@ -12,17 +12,55 @@ public class MyNegotiationStrategy {
 	}
 
 	public boolean selectAccept(Bid lastReceivedOffer, double time) {
-		double utility = utilitySpace.getUtility(lastReceivedOffer);
-		return utility > 0.9d;
+		double u = utilitySpace.getUtility(lastReceivedOffer);
+		double utility = 0;
+		if (time <= 0.25D) {
+			utility = 1.0D - time * 0.4D;
+		}
+		if ((time > 0.25D) && (time <= 0.375D)) {
+			utility = 0.9D + (time - 0.25D) * 0.4D;
+		}
+		if ((time > 0.375D) && (time <= 0.5D)) {
+			utility = 0.95D - (time - 0.375D) * 0.4D;
+		}
+		if ((time > 0.5D) && (time <= 0.6D)) {
+			utility = 0.9D - (time - 0.5D);
+		}
+		if ((time > 0.6D) && (time <= 0.7D)) {
+			utility = 0.8D + (time - 0.6D) * 2.0D;
+		}
+		if ((time > 0.7D) && (time <= 0.8D)) {
+			utility = 1.0D - (time - 0.7D) * 3.0D;
+		}
+		if ((time > 0.8D) && (time <= 0.9D)) {
+			utility = 0.7D + (time - 0.8D) * 1.0D;
+		}
+		if ((time > 0.9D) && (time <= 0.95D)) {
+			utility = 0.8D - (time - 0.9D) * 6.0D;
+		}
+		if (time > 0.95D) {
+			utility = 0.5D + (time - 0.95D) * 4.0D;
+		}
+		if (time > 1) {
+			utility = 0.8;
+		}
+		if(time>0.99){
+
+		}
+		System.out.println("测试"+utility);
+		//		if (time > 0.99) {
+		//			utility=utilitySpace.getReservationValue();
+		//		}
+		return u > utility;
 	}
 
 	public boolean selectEndNegotiation(double time) {
-		
+
 		return false;
 	}
 
 	public Action OfferAction() {
-		
+
 		return null;
 	}
 }
